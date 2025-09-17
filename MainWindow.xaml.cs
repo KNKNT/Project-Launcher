bf81475d4b39ac42d9097c972813ea0fc67b6071
Ilya Koshman <radirmail044@gmail.com>
2025-09-17 18:53:25 +0300

    update 1.1

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,55 +21,23 @@ namespace Project_Launcher
     /// </summary>
     public partial class MainWindow : Window
     {
+        string treeUid;
+        string treeItemUid;
+        public TreeViewItem selectedItem { get; set; }
         public MainWindow()
         {
             InitializeComponent();
-            LoadData();
         }
-
-        private void LoadData()
+        int categoriesCount = 0;
+        public void categoriesAdd(object sender, RoutedEventArgs e) =>
+        categoiesPanel.Items.Add(new TreeViewItem { Header = $"Kategoiya {++categoriesCount}" });
+        public void categoriesDel(object sender, RoutedEventArgs a) =>
+        (selectedItem.Parent as ItemsControl).Items.Remove(selectedItem);
+        private void categoiesPanel_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
         {
-            
-            var projects = new List<ApplicationCard>
-            {
-                new ApplicationCard { },
-
-                new ApplicationCard
-                {
-                    Color = "#4bac6f",
-                    Name = "Экономическая повестка сегодняшнего дня сделала своё дело Экономическая повестка сегодняшнего дня сделала своё дело Экономическая повестка сегодняшнего дня сделала своё дело",
-                    Discription = "Кстати, диаграммы связей представляют собой не что иное, как квинтэссенцию победы маркетинга над разумом и должны быть заблокированы в рамках своих собственных рациональных ограничений. Но активно развивающиеся страны третьего мира и по сей день остаются уделом либералов, которые жаждут быть разоблачены. В частности, повышение уровня гражданского сознания говорит о возможностях первоочередных требований. Картельные сговоры не допускают ситуации, при которой ключевые особенности структуры проекта, которые представляют собой яркий пример континентально-европейского типа политической культуры, будут представлены в исключительно положительном свете. Однозначно, реплицированные с зарубежных источников, современные исследования освещают чрезвычайно интересные особенности картины в целом, однако конкретные выводы, разумеется, ограничены исключительно образом мышления!",
-                    Path = @"C:\Program Files (x86)\Common Files\Microsoft\ExtensionManager\Extensions",
-                    FilesCount = 1500000,
-                    ChangeDate = DateTime.Now.AddDays(-5)
-                },
-                new ApplicationCard
-                {
-                    Color = "#4bac6f",
-                    Name = "Экономическая повестка сегодняшнего дня сделала своё дело Экономическая повестка сегодняшнего дня сделала своё дело Экономическая повестка сегодняшнего дня сделала своё дело",
-                    Discription = "Кстати, диаграммы связей представляют собой не что иное, как квинтэссенцию победы маркетинга над разумом и должны быть заблокированы в рамках своих собственных рациональных ограничений. Но активно развивающиеся страны третьего мира и по сей день остаются уделом либералов, которые жаждут быть разоблачены. В частности, повышение уровня гражданского сознания говорит о возможностях первоочередных требований. Картельные сговоры не допускают ситуации, при которой ключевые особенности структуры проекта, которые представляют собой яркий пример континентально-европейского типа политической культуры, будут представлены в исключительно положительном свете. Однозначно, реплицированные с зарубежных источников, современные исследования освещают чрезвычайно интересные особенности картины в целом, однако конкретные выводы, разумеется, ограничены исключительно образом мышления!",
-                    Path = @"C:\Program Files (x86)\Common Files\Microsoft\ExtensionManager\Extensions",
-                    FilesCount = 1500000,
-                    ChangeDate = DateTime.Now.AddDays(-5)
-                },
-                new ApplicationCard
-                {
-                    Color = "#FF808080",
-                    Name = "Проект 2",
-                    Discription = "Второй проект",
-                    Path = @"C:\Project2",
-                    FilesCount = 27,
-                    ChangeDate = DateTime.Now.AddDays(-3),
-                }
-            };
-
-            foreach (var project in projects)
-            {
-                var card = new Card();
-                card.DataContext = project; 
-                CardsPanel.Children.Add(card);
-            }
+            if (e.NewValue is TreeViewItem tree)
+                (selectedItem, treeItemUid) = (tree, tree.Uid);
         }
-        
+
     }
 }
