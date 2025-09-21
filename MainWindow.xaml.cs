5c776c81794303b1d926ddbd128762b92e2f12c8
Ilya Koshman <radirmail044@gmail.com>
2025-09-21 17:42:51 +0300

    1.22

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using Project_Launcher.UIElements;
 using System;
-
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -12,12 +12,12 @@ namespace Project_Launcher
     /// </summary>
     public partial class MainWindow : Window
     {
-        int categoriesCount = 0;
+        int upperCount = 0;
+        int underCount = 0;
         string header;
         public TreeViewItem selectedItem { get; set; }
         public MainWindow() => InitializeComponent();
         private void categoriesDel(object sender, RoutedEventArgs a) => (selectedItem.Parent as ItemsControl).Items.Remove(selectedItem);
-        private void AddCardButton_Click(object sender, RoutedEventArgs e) => selectedItem.Items.Add(new TreeViewItem { Header = header });
         private void categoiesPanel_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e) => selectedItem = e.NewValue as TreeViewItem;
         public void rewriteText(string _text) => selectedItem.Header = _text;
         private void RenameButton_Click(object sender, RoutedEventArgs e) => RenameField.Visibility = (RenameField.Visibility != Visibility.Visible)
@@ -25,8 +25,14 @@ namespace Project_Launcher
         : Visibility.Hidden;
         public void categoriesAdd(object sender, RoutedEventArgs e)
         {
-            header = $"Kategoiya {++categoriesCount}";
-            categoiesPanel.Items.Add(new TreeViewItem { Header = header });
+            header = $"Kategoiya {upperCount++}";
+            TreeViewItem treeViewItem = new TreeViewItem
+            {
+                Header = header,
+                Uid = $"{underCount++}"
+            };
+            categoiesPanel.Items.Add(treeViewItem);
+            treeViewItem.MouseDoubleClick += TreeViewItem_Selected;
             RenameField.NameTextBox.Text = header;
         }
 
